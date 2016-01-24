a648c691f44090a26364651c3b06b3a4824ae8ec
rafeh01 <rafehqazi1@gmail.com>
2016-01-24 02:31:37 -0600

    added ability to create completed undirected graphs from given number of nodes.

--- algorithmic_thinking_part1/week1/project/degree_distributions_for_graphs.py
+++ algorithmic_thinking_part1/week1/project/degree_distributions_for_graphs.py
@@ -2,6 +2,8 @@
 # Author: Rafeh Qazi
 # Date: 01/18/2016
 
+import itertools
+
 
 EX_GRAPH0 = {
     0: {1, 2},
@@ -44,4 +46,39 @@ def make_complete_graph(num_nodes):
     :return: dict
     """
     max_edges = (num_nodes * (num_nodes - 1)) // 2
-    
+    combos = list(itertools.combinations(range(num_nodes), 2))
+    all_nodes = {}
+    range_ = range(num_nodes)
+    for combo in combos:
+        if combo[0] not in all_nodes:
+            all_nodes[combo[0]] = {combo[1]}
+        else:
+            all_nodes[combo[0]].add(combo[1])
+
+    # Figure out the range to set to 0 nodes.
+    for i in range_:
+        if i not in all_nodes:
+            range_ = range(i, num_nodes)
+            break
+
+    # Assign that range to 0 nodes or an empty set.
+    for i in range_:
+        all_nodes[i] = {}
+    return all_nodes
+
+
+def playground():
+    x = list(itertools.combinations(['v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7', 'v8'], 2))
+    max_edges = (8 * (8 - 1)) // 2
+    print(max_edges)
+    print(len(x))
+    a = {}
+    key = "somekey"
+    a.setdefault(key, [])
+    a[key].append(1)
+    print(x)
+
+
+if __name__ == '__main__':
+    print(make_complete_graph(8))
+    print(make_complete_graph(3))
